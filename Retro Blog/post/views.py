88ac6d6d7f950c1000ab0b9389e017ccb79a435b
itmadityajoshi <adityajoshi316@gmail.com>
2025-09-08 22:12:39 +0200

    fetch data from backend to frontend

--- Retro Blog/post/views.py
+++ Retro Blog/post/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from .models import Category
 
 # Create your views here.
 def post(req):
-    return render(req, 'post.html')
+    categories= Category.objects.all()
+    context = {
+        'categories': categories
+    }
+    return render(req, 'post.html', context)
 
 
 def worldPost(req):
